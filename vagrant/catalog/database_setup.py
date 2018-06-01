33fb0a19178b65357534b007954d0888deb94447
Vivek Lingayat <vvk.vivek.v@gmail.com>
2018-06-01 09:07:50 +0530

    init

--- vagrant/catalog/database_setup.py
+++ vagrant/catalog/database_setup.py
@@ -8,34 +8,47 @@ from sqlalchemy import create_engine
 Base = declarative_base()
 
 
-class Sport(Base):
-    __tablename__ = 'sport'
+class User(Base):
+    __tablename__ = 'user'
 
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
 
-    @property
-    def serialize(self):
-        """Return object data in easily serializeable format"""
-        return {
+
+class Sport(Base):
+	__tablename__ = 'sport'
+
+	id = Column(Integer, primary_key=True)
+	name = Column(String(250), nullable=False)
+	user_id = Column(Integer, ForeignKey('user.id'))
+	user = relationship(User)
+
+	@property
+	def serialize(self):
+		"""Return object data in easily serializeable format"""
+		return {
             'name': self.name,
             'id': self.id,
         }
 
 
 class SportItem(Base):
-    __tablename__ = 'sport_item'
+	__tablename__ = 'sport_item'
 
-    name = Column(String(80), nullable=False)
-    id = Column(Integer, primary_key=True)
-    description = Column(String(250))
-    sport_id = Column(Integer, ForeignKey('sport.id'))
-    sport = relationship(Sport)
+	name = Column(String(80), nullable=False)
+	id = Column(Integer, primary_key=True)
+	description = Column(String(250))
+	sport_id = Column(Integer, ForeignKey('sport.id'))
+	sport = relationship(Sport)
+	user_id = Column(Integer, ForeignKey('user.id'))
+	user = relationship(User)
 
-    @property
-    def serialize(self):
-        """Return object data in easily serializeable format"""
-        return {
+	@property
+	def serialize(self):
+		"""Return object data in easily serializeable format"""
+		return {
             'name': self.name,
             'description': self.description,
             'id': self.id,
